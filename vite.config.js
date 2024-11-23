114633153339d7b730d340351fade503ce500b59
Rico <4007513+loschke@users.noreply.github.com>
2024-11-23 23:22:43 +0100

    add dockerfile

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    server: {
+        host: true, // Needed for container environment
+        strictPort: true
+    },
+    preview: {
+        host: true, // Needed for container environment
+        strictPort: true
+    }
 });
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: true, // Needed for container environment
+    strictPort: true
+  },
+  preview: {
+    host: true, // Needed for container environment
+    strictPort: true
+  }
 })
